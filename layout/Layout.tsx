c0d36da870f630b5fccf59d478666d5bc43274f0
Dennis-Codes <dennis@ngeni.io>
2023-05-11 03:56:54 +0300

    fixed title

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { Footer, Navbar } from "@/components";
+import { Html } from "next/document";
 import Head from "next/head";
 
 interface LayoutProps {
@@ -9,7 +10,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Head>
-        <title>Meme Tokens</title>
+        <title>Bob Pepe Ai</title>
         <meta name="description" content="Some Make memes Some Make Millions" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
